b0ab728979fa8463223f0fabdde7e4670a442153
artemObrazumov <a.obraz60022006@gmail.com>
2025-01-27 14:51:33 +0300

    (leftover) fixed error codes and implemented ordering in stats

--- src/main/java/com/artemobrazumov/shorty/short_url/advice/GlobalExceptionHandler.java
+++ src/main/java/com/artemobrazumov/shorty/short_url/advice/GlobalExceptionHandler.java
@@ -1,11 +1,25 @@
 package com.artemobrazumov.shorty.short_url.advice;
 
+import com.artemobrazumov.shorty.short_url.exceptions.NotShortUrlAuthorException;
+import com.artemobrazumov.shorty.short_url.exceptions.ShortUrlNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,4 +31,38 @@ public class GlobalExceptionHandler {
     public void handleException(Exception ex) {
         logger.error("An error occurred: {}", ex.getMessage(), ex);
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handleNotFoundError() {
+        return "404";
+    }
+
+    @ExceptionHandler(ShortUrlNotFoundException.class)
+    public ResponseEntity<String> handleShortUrlNotFoundException(ShortUrlNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotShortUrlAuthorException.class)
+    public ResponseEntity<Object> handleNotShortUrlAuthorException(NotShortUrlAuthorException ex) {
+        Map<String, String> body = new HashMap<>();
+        body.put("error", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> body = new HashMap<>();
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 }
+
